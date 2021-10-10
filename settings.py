0b694d67b67f3a4c1d9762f5dd11cd7048bd664b
Don-CorliJoni <haueis.jonathan@gmail.com>
2021-10-10 21:10:53 +0200

    Added other settings

--- settings.py
+++ settings.py
@@ -1,3 +1,22 @@
-
 def get_configuration_folder():
     return "Data/Highscores"
+
+
+def get_test_data_path_small():
+    return "Data/TestData/mnist_test_10.csv"
+
+
+def get_test_data_path_big():
+    return "Data/TestData/mnist_test.csv"
+
+
+def get_own_test_data_path():
+    return "Data/TestData/my_train_data.csv"
+
+
+def get_train_data_path_small():
+    return "Data/TrainingsData/mnist_train_100.csv"
+
+
+def get_train_data_path_big():
+    return "Data/TrainingsData/mnist_train.csv"
