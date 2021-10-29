4250811b7976d27faa5025e6ccd5ab6d1145df64
Don-CorliJoni <haueis.jonathan@gmail.com>
2021-10-29 21:57:43 +0200

    Stand 29.10.2021

--- controller.py
+++ controller.py
@@ -1,5 +1,9 @@
-# from NeuralNetzworkHelper import run_test_with_threads, run_test_parallel
-# from AIConfig import AIConfigHandler
+from ai_highscore.ai_configuration_handler.file_handler import FileHandler
+from neural_network_helper import NeuralNetworkEvaluator
+from settings import get_configuration_folder
+from concurrent.futures import ProcessPoolExecutor
+from threading import Thread
+import json
 
 
 class Controller:
@@ -14,11 +18,16 @@ class Controller:
         self.run_task()
         self.finalize()
 
-    def _initialize(self):
-        pass
 
-    def _run(self):
-        pass
+def _initialize():
+    FileHandler.load_configs(get_configuration_folder())
 
-    def _finalize(self):
-        pass
+
+def _run():
+    with open('Data/TestConfigurationData/TestConfigData-HighHNeurons+LowLR+HighTrainE.json') as f:
+        data = json.load(f)
+        # test_data["test_epochs"]
+        # test_data["training_configurations"]
+
+def _finalize():
+    FileHandler.save_configs(get_configuration_folder())
--- neural_network_helper/__init__.py
+++ neural_network_helper/__init__.py
@@ -1,4 +1,6 @@
 from .scoreboard import Scoreboard
 from .neural_network_evaluator import NeuralNetworkEvaluator
-from neural_network_evaluator_predefinitions import train_and_query_big, train_and_query_big_once, \
+from .neural_network_evaluator_predefinitions import train_and_query_big, train_and_query_big_once, \
     train_and_query_small, train_and_query_small_once
+from .neural_network_tester import run_test_parallel
+
